c75217a54ca97d1fb058278ead57d169c0001b43
gourav soni <gouravsoni8476@gmail.com>
2024-06-07 23:22:58 +0530

    added middleware to auth admin

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,23 @@
+import { Admin } from "../db/index.js";
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+async function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const { username, password } = req.headers;
+  await Admin.find({
+    username: username,
+    password: password,
+  })
+    .then((admin) => {
+      if (admin.length > 0) {
+        next();
+      } else {
+        console.log("no user found with this username and password");
+        return false;
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 export default adminMiddleware;
