6fe28a35d66e6e04617855b4b2283249ec3f190f
gourav soni <gouravsoni8476@gmail.com>
2024-06-08 20:18:35 +0530

    done userMiddleware

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import bodyParser from 'body-parser'
 const app = express();
 import adminRouter from "./routes/admin.js";
-// import userRouter from "./routes/user.js";
+import userRouter from "./routes/user.js";
 
 // Middleware for parsing request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/admin", adminRouter);
-// app.use("/user", userRouter);
+app.use("/user", userRouter);
 
 const PORT = 3000;
 
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,16 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+import { User } from "../db/index.js";
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const { username, password } = req.headers;
+  await User.find({ username, password })
+    .then((user) => {
+      next();
+    })
+    .catch((err) => {
+      console.log("log from userMiddleware", err);
+    });
 }
 
-module.exports = userMiddleware;
+export default userMiddleware;
