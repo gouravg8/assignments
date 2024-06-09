524d4993784f9d7b01a8869bb2c5bcf5504434a0
gourav soni <gouravsoni8476@gmail.com>
2024-06-09 14:48:11 +0530

    done signup, signin of admin

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,68 @@
-const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+import { Router } from "express";
+import adminMiddleware from "../middleware/admin.js";
 const router = Router();
+import { Admin } from "../db/index.js";
+import jwt from "jsonwebtoken";
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  try {
+    if (!(username && password)) {
+      res.status(400).json({ message: "Pleaser enter username and password" });
+    }
+    const adminExisted = Admin.findOne({ username, password });
+    if (adminExisted) {
+      res.status(409).json({ message: "Admin already existed" });
+    } else {
+      await Admin.create({ username, password }).then(() => {
+        const token = jwt.sign({ username }, process.env.JWTPASSKEY);
+        res.json({ message: "Admin created successfully", token });
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // get all data from user
+  // all the data should be existed
+  // chech if user already existed
+  // encrypt the password
+  // save the user data in db
+  // generate the token for user and send it
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  try {
+    if (!(username && password)) {
+      res.status(400).json({ message: "Pleaser enter username and password" });
+    }
+    const admin = await Admin.findOne({ username, password });
+    if (admin) {
+      const token = jwt.sign({ username }, process.env.JWTPASSKEY);
+      console.log(req.cookies);
+
+      let options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      res.cookie("token", token, options).json({ success: true, token, admin });
+    } else {
+      res.json({ message: "No user found" });
+    }
+  } catch (error) {
+    res.json({ message: "error: " + error });
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
-module.exports = router;
+export default router;
