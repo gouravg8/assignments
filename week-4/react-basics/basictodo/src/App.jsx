3a5e5c7eb25b24f2f87a4846dfa3f7058670887d
gourav soni <gouravsoni8476@gmail.com>
2024-06-15 23:38:16 +0530

    doing basic todo app

--- week-4/react-basics/basictodo/src/App.jsx
+++ week-4/react-basics/basictodo/src/App.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
+// function Textcompo() {
+//   return (
+//     <div>
+//       <h3>Hello from home</h3>
+//     </div>
+//   );
+// }
+// function Todo() {
+//   return (
+//     <ul>
+//       <li>My faltu ka todo</li>
+//     </ul>
+//   );
+// }
 const App = () => {
   const [inputTodo, setInputTodo] = useState({ text: "suar", id: "1" });
   const [todoList, setTodoList] = useState([]);
+
   const todos = [
     {
       title: "Go to gym",
@@ -15,16 +31,34 @@ const App = () => {
       id: 2,
     },
   ];
-  const handleSubmit = () => {
+  useEffect(() => {
+    // fetch("https://sum-server.100xdevs.com/todos")
+    //   .then((res) => res.json())
+    //   .then((data) => setTodoList(data));
+    setTodoList(todos);
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(todoList);
-    setTodoList([inputTodo, ...todoList]);
-    setInputTodo({ text: "", id: "" });
+    if (inputTodo.text) {
+      setTodoList([inputTodo, ...todoList]);
+      setInputTodo({ text: "", id: "" });
+    }
+  };
+  const deleteTodo = (e) => {
+    e.target.parentElement.remove();
+  };
+  const updateTodo = (e) => {};
+  const liStyle = {
+    display: "flex",
+    gap: "15px",
   };
   return (
     <div>
       <h1>This is the basic todo</h1>
-      <div>
+      {/* <div>
         <input
+          style={{ margin: "5px", padding: "2px 4px", borderRadius: "5px" }}
           type="text"
           name="inp"
           id="inp"
@@ -33,15 +67,50 @@ const App = () => {
             setInputTodo({ text: e.target.value, id: Math.random() * 100 })
           }
         />
-        <button onClick={handleSubmit}>add</button>
-      </div>
+        <button
+          onClick={handleSubmit}
+          style={{ margin: "5px", padding: "2px 4px", borderRadius: "3px" }}
+        >
+          add
+        </button>
+      </div> */}
       <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+        {todoList.map(({ title, id }) => (
+          <li key={id} style={liStyle}>
+            {title}
+            <button onClick={deleteTodo}>X</button>
+            <button onClick={updateTodo}>edit</button>
+          </li>
         ))}
       </ul>
+      {/* <WraperCompo>
+        <Textcompo />
+        <Todo />
+      </WraperCompo> */}
     </div>
   );
 };
+
+// function WraperCompo({ children }) {
+//   return (
+//     <div
+//       style={{
+//         border: "2px solid black",
+//         borderRadius: "5px",
+//         fontSize: "20px",
+//         fontFamily: "verdana",
+//       }}
+//     >
+//       {children}
+//     </div>
+//   );
+// }
+
+// const Todo = React.memo(({ text, id }) => {
+//   <li key={id}>
+//     {text} <button onClick={deleteTodo}>X</button>
+//     <button onClick={editTodo}>edit</button>
+//   </li>;
+// });
 
 export default App;
