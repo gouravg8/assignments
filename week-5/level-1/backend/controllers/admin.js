43e3d3e2e625ad464a696605eee843cac399db97
gourav soni <gouravsoni8476@gmail.com>
2024-06-22 20:03:05 +0530

    created user, card controllers2

--- week-5/level-1/backend/controllers/admin.js
+++ week-5/level-1/backend/controllers/admin.js
@@ -21,22 +21,19 @@ const createAdmin = async (req, res) => {
   }
 };
 
-const signinAdmin = async(req, res)=>{
+const signinAdmin = async (req, res) => {
   const { username, password } = req.body;
 
-
-  const userExisted = await Admin.findOne({ username })
-  if (userExisted) {
-    req.session.username = userExisted.username;
-    req.session.password = userExisted.password;
-    res.json({ message: "Admin already existed" });
-  } else {
-    await Admin.create({ username, password: hashedPassword }).then((user) => {
+  const userExisted = await Admin.findOne({ username }).then(async (user) => {
+    const samePass = await bcrypt.compare(password, user.password);
+    // console.log({ user, hashedPassword });
+    if (!samePass) {
+      res.status(403).json({ message: "Invalid credential" });
+    } else {
       req.session.username = user.username;
       req.session.password = user.password;
-      res.json({ message: "Admin created with username " + username });
-    });
-  }
-
-}
+      res.json({ message: "Logged in...", reqq: req.session });
+    }
+  });
+};
 export { createAdmin, signinAdmin };
