c898ddf1b96d71f510bafcc7b1d29fa41c5a6abb
gourav soni <gouravsoni8476@gmail.com>
2024-06-22 14:15:12 +0530

    done userverif middlware

--- week-5/level-1/backend/middleware/userVerif.js
+++ week-5/level-1/backend/middleware/userVerif.js
@@ -1,15 +1,17 @@
 import { Admin } from "../db/db.js";
+
 async function userVerification(req, res, next) {
   const { username, password } = req.headers;
   await Admin.findOne({ username, password })
     .then((admin) => {
       if (admin) {
         console.log("user found");
-        res.headers.set("username", username);
+        req.session.username = username;
+        req.session.password = password;
         next();
-      }
-      throw Error("User not found");
+      } else throw Error("User not found");
     })
-    .catch((err) => res.json({ err }));
+    .catch((err) => res.json({ err: err.message }));
 }
+// }
 export default userVerification;
