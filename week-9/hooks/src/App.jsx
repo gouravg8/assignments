fe97f863fb4e89276e4d3f1cb09e3728a988cf4c
gourav soni <gouravsoni8476@gmail.com>
2024-06-26 12:43:14 +0530

    component intentionally unmounts

--- week-9/hooks/src/App.jsx
+++ week-9/hooks/src/App.jsx
@@ -1,16 +1,58 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { Component, useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isDelay, setIsDelay] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsDelay(false);
+    }, 5000);
+  });
 
   return (
     <>
-      <div></div>
+      <div>
+        {isDelay && <MyCompo />}
+        <MyCompo2 />
+      </div>
     </>
   );
+}
+const MyCompo = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+
+  useEffect(() => {
+    console.log("component mounts");
+    return () => {
+      console.log("compon Unmounts");
+    };
+  }, []);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={handleClick}>increase</button>
+    </div>
+  );
+};
+
+export class MyCompo2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.increment}>increase</button>
+      </div>
+    );
+  }
 }
 
 export default App;
