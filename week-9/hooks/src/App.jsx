108823bcbd564bfa7cec9d017ef3cb3e639bfcbc
gourav soni <gouravsoni8476@gmail.com>
2024-06-27 13:17:36 +0530

    minor changes

--- week-9/hooks/src/App.jsx
+++ week-9/hooks/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 import { useEffect } from "react";
+import Todo from "./components/Todo";
 
 function App() {
   const [isDelay, setIsDelay] = useState(false);
@@ -15,52 +16,10 @@ function App() {
       <div>
         {/* {isDelay && <MyCompo />} */}
         {/* {isDelay && <MyCompo2 />} */}
+        <Todo />
       </div>
     </>
   );
-}
-const MyCompo = () => {
-  const [count, setCount] = useState(0);
-  const handleClick = () => setCount(count + 1);
-
-  useEffect(() => {
-    console.log("component mounts");
-    return () => {
-      console.log("compon Unmounts");
-    };
-  }, []);
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={handleClick}>increase</button>
-    </div>
-  );
-};
-
-export class MyCompo2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  componentDidMount() {
-    console.log("mounted 2");
-  }
-  componentWillUnmount() {
-    console.log("unmounted 2");
-  }
-  render() {
-    return (
-      <div>
-        <p>compo 2</p>
-        {/* <p>{this.state.count}</p> */}
-        {/* <button onClick={this.increment}>increase</button> */}
-      </div>
-    );
-  }
 }
 
 export default App;
