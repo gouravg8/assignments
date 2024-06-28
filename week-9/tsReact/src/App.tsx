241438f8760469eaafdae07b6976655d9eeb82a9
gourav soni <gouravsoni8476@gmail.com>
2024-06-28 20:43:46 +0530

    done interface

--- week-9/tsReact/src/App.tsx
+++ week-9/tsReact/src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <div></div> 
-    </>
-  )
+interface Todo {
+  id: number;
+  title: string;
+  description: string;
 }
 
-export default App
+const todos: Todo[] = [
+  { id: 1, title: "todo1", description: "desc 1" },
+  { id: 2, title: "todo2", description: "desc 2" },
+  { id: 3, title: "todo3", description: "desc 3" },
+];
+function App() {
+  return (
+    <>
+      <div>
+        <Todo todo={todos} />
+      </div>
+    </>
+  );
+}
+
+const Todo = ({ todo }: { todo: Todo[] }) => {
+  return (
+    <>
+      {todo.map((t: Todo) => (
+        <p key={t.id}>
+          {t.title}, {t.description}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default App;
