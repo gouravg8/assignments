a53c50d9abf848a8ff233235fe7f8aa63ba8aba6
gourav soni <gouravsoni8476@gmail.com>
2024-06-29 22:18:58 +0530

    learning sql queries for postgres

---
+++ week-9/pg/week-13-sql/src/utils.ts
@@ -0,0 +1,8 @@
+import { Client } from "pg";
+
+export async function getClient() {
+  const client = new Client("postgres://postgres:gourav@localhost:5432/todog");
+  // const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+  await client.connect();
+  return client;
+}
